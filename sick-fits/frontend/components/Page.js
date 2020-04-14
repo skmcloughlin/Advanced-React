b1eb437b38253e0e62ecb6bc96153d4467d73892
skmcloughlin <skmcloughlin@gmail.com>
2020-04-14 09:35:23 -0400

    Base structure finished

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Nav from './Nav'
+import Meta from './Meta'
 
 export default class Page extends Component {
     render() {
         return (
             <div>
+                <Meta />
                 <Header />
                 <Nav />
                 {this.props.children}
